d58512c7ca41f13eb6988dd884a9521f7926cd04
emguse <30695541+emguse@users.noreply.github.com>
2023-11-05 15:03:11 +0900

    format adjust

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,3 +11,8 @@ def index():
 @app.route("/hello/<string:name>", methods=["GET", "POST"], endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
